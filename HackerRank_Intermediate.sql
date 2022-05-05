01254e85534166bf3bfd7590c41b662d576d0b7a
vapb <vapb7123@gmail.com>
2022-05-05 14:14:44 -0300

    HackerRankIntermediate 20%

--- HackerRank_Intermediate.sql
+++ HackerRank_Intermediate.sql
@@ -24,3 +24,40 @@ FROM
     FROM station
     ORDER BY lat_n) AS l 
 WHERE l.rowindex IN (floor(@rowindex/2), ceil(@rowindex/2));
+
+
+-- Binary Tree Nodes
+-- You are given a table, BST, containing two columns: N and P, where N represents the value of a node in Binary Tree, and P is the parent of N.
+SELECT N,
+    case
+        when P IS NULL then 'Root'
+        when (N IN (select P from BST where P IS NOT NULL)) then 'Inner'
+        else 'Leaf'
+    end
+FROM BST
+ORDER BY N;
+
+
+-- Challenges
+-- Julia asked her students to create some coding challenges. Write a query to print the hacker_id, name, and the total number of challenges created by each student. Sort your results by the total number of challenges in descending order. If more than one student created the same number of challenges, then sort the result by hacker_id. If more than one student created the same number of challenges and the count is less than the maximum number of challenges created, then exclude those students from the result.
+SELECT c.hacker_id as id, h.name as name, count(c.hacker_id) as counter
+FROM Hackers h
+JOIN Challenges c on c.hacker_id = h.hacker_id
+GROUP BY c.hacker_id, h.name
+-- Discover Max count
+HAVING counter = ( 
+    SELECT MAX(temp.cnt)
+    FROM(SELECT count(hacker_id) as cnt
+        FROM Challenges 
+        GROUP BY hacker_id)
+    as temp)
+
+-- Discover count with only 1
+    OR counter IN ( 
+    SELECT tmp.cnt
+    FROM (SELECT hacker_id, count(hacker_id) as cnt
+                FROM Challenges 
+                GROUP BY hacker_id) as tmp
+    GROUP BY tmp.cnt
+    HAVING count(*) = 1)
+ORDER BY counter DESC, id;
